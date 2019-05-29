48c89fa387e8e2b3c5dc299a9b692ff5e9d7baa8
RockLucca <luccatbordim@gmail.com>
2019-05-29 11:29:13 -0300

    Entrada

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
+	int n1, n2,n;
+
+	//entrada
+	printf("Digite o primeiro numero inteiro:");
+	scanf("%d",&n1);
+	printf("Digite o segundo numero:");
+	scanf("%d",&n2);
+	printf("Digite 0 para soma e 1 para subtração:");
+	scanf("%d",&n);
 	switch(n){
 	case 0:
 		break;
