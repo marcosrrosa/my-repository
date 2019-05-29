a57d182adc3285c1fdea4fd9f01caf7381fe9383
marcosrrosa <marcosrosa@alunos.utfpr.edu.br>
2019-05-29 11:26:30 -0300

    funçao de sub

--- main.c
+++ main.c
@@ -4,6 +4,10 @@ int main(){
 	case 0:
 		break;
 	case 1:
+		printf("função de subtração escolhida!\n")
+		res = n1 - n2;
+		printf("O resultado da subtração é de : %d\n", res);
+
 		break;
 	default: break;
 	}
